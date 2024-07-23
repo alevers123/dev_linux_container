8e0a07d70c568330ab2986b870dbaaeae3705be1
Alexander Evers <alex-evers@gmx.de>
2024-07-24 01:06:47 +0200

    Further config files added

---
+++ tmux.lua
@@ -0,0 +1,11 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  enabled = true,
+  event = "VeryLazy",
+  keys = {
+    { "<c-h>", "<cmd>TmuxNavigateLeft<cr>",  "Navigate Window Left" },
+    { "<c-j>", "<cmd>TmuxNavigateDown<cr>",  "Navigate Window Down" },
+    { "<c-k>", "<cmd>TmuxNavigateUp<cr>",    "Navigate Window Up" },
+    { "<c-l>", "<cmd>TmuxNavigateRight<cr>", "Navigate Window Right" },
+  },
+}
